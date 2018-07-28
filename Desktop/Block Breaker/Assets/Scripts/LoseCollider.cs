64e6a8a3938e39198544256cbf89d58801a5a755
Jeffrey <JeffreyPadgett1@gmail.com>
2018-07-27 20:10:16 -0700

    2nd commit improvements

---
+++ Desktop/Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    private LevelManager LevelManager;
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        LevelManager = GameObject.FindObjectOfType<LevelManager>();
+        LevelManager.LoadLevel("Lose Screen");
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+
+    }
+}
---
+++ Desktop/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool autoPlay = false;
+    private bool hasStarted = false;
+    private Ball ball;
+    public float minX, maxX;
+    // Use this for initialization
+    void Start()
+    {
+        ball = GameObject.FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (autoPlay == false)
+        {
+            MoveWithMouse();
+        }
+        else
+        {
+            AutoPlay();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+        }
+
+    }
+
+    void AutoPlay()
+    {
+        Vector3 ballPos = ball.transform.position;
+        Vector3 paddlePos = new Vector3(7.99f, this.transform.position.y, 0f);
+        paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);
+        this.transform.position = paddlePos;
+    }
+
+    void MoveWithMouse()
+    {
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        Vector3 paddlePos = new Vector3(7.99f, this.transform.position.y, 0f);
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
+        this.transform.position = paddlePos;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (hasStarted)
+        {
+            GetComponent<AudioSource>().Play();
+
+        }
+    }
+}
