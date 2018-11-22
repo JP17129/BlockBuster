05bda110c4c88c4ac58dccc8d06a2026f11768a4
Jeffrey <JeffreyPadgett1@gmail.com>
2018-11-22 05:38:34 -0800

    Database first migration code first

--- Desktop/repository_files/ASP.NET/Vidly/Vidly/App_Start/RouteConfig.cs
+++ Desktop/repository_files/ASP.NET/Vidly/Vidly/App_Start/RouteConfig.cs
@@ -12,16 +12,20 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapMvcAttributeRoutes();
             //****************OLD WAY****************        
             //routes.MapRoute(
             //    "MoviesByReleaseDate",                                                   //name of the parameter. MapRoute takes several parameters but the most common used takes three.
             //    "movies/released/{year}/{month}",                                        //URL order, the curly braces are the parameters
             //    new {controller = "Movies", action = "ByReleaseDate"},                   //Defaults
             //    new {year = @"\d{4}", month = @"\d{2}"});  
-                   
 
+            routes.MapMvcAttributeRoutes();                 // <---- New Way that is shown in movies/random
+
+            routes.MapRoute(
+                "Customer",
+                "customer/index/{id}",
+                new {controller = "customer", action = "Index", id = UrlParameter.Optional}
+                );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
